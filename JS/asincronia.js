218043a219ff367fafa8884cfb73e9f9961c2460
juan_zupel <juanzupel21@gmail.com>
2026-08-04 14:58:37 -0300

    Clase 02 - Agrego estructura JS y primer ejemplo de asincronia

---
+++ JS/asincronia.js
@@ -0,0 +1,61 @@
+// console.log("Inicio");
+// setTimeout(() => {
+//     console.log(".......");   
+// }, 3000);
+// setTimeout(() => {
+//     console.log("Fin");
+// }, 5000);
+
+// // Tambien se puede...
+
+// console.log("Inicio");
+// setTimeout(() => {
+//     console.log(".......");   
+//     setTimeout(() => {
+//     console.log("Fin");
+// }, 5000);
+// }, 3000);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// function saludar() {
+//     console.log("Hola");
+// }
+// function ejecutar(funcion) {
+//     funcion();
+// }
+
+// ejecutar(saludar);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log("AM PATRICK AND I LOVE POTATO CHIPS");
+// setTimeout(() => {
+//     console.log("Patrick, I´m out");
+// }, 1000);
+// setTimeout(() => {
+//     console.log("I´m out");
+// }, 2000);
+// setTimeout(() => {
+//     console.log("AM AUT...");
+// }, 4000);
+// setTimeout(() => {
+//     console.log("AM AUT...");
+// }, 5000);
+// setTimeout(() => {
+//     console.log("AAAUWRUWUURUUURURUUWWUAAAWAWAWWWURU");
+// }, 6000);
+// setTimeout(() => {
+//     console.log("AAAAAHHHHHHHHHHHHHH");
+// }, 8000);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log("Abriendo SGA...");
+// setTimeout(() => {
+//     console.log("Alumnos Cargados")
+// }, 3000);
+
+// console.log("El usuario puede seguir navegando");
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
