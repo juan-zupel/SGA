4d8399a2889315bbb3daab0c722314fc9efacb3d
juan_zupel <juanzupel21@gmail.com>
2026-08-11 15:54:20 -0300

    Clase 06 - alta y persistencia de alumnos

--- JS/alumnos.js
+++ JS/alumnos.js
@@ -63,32 +63,32 @@
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-async function cargarAlumnos() {
-    const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
-    const alumnos = await respuesta.json();
-    console.log(alumnos);
-    return alumnos;
-}
+// async function cargarAlumnos() {
+//     const respuesta = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const alumnos = await respuesta.json();
+//     console.log(alumnos);
+//     return alumnos;
+// }
 
-function mostrarAlumnos(alumnos) {
-    // console.table(alumnos);
-    console.log(typeof alumnos);
-    localStorage.setItem("alumnos", JSON.stringify(alumnos));
-    const datos = localStorage.getItem("alumnos");
-    console.log(typeof datos);
-    console.log(datos);
-    const alumnosRecuperados = JSON.parse(datos);
-    console.log(typeof(alumnosRecuperados));
-    console.log(alumnosRecuperados);
-    console.table(alumnosRecuperados);
-}
+// function mostrarAlumnos(alumnos) {
+//     // console.table(alumnos);
+//     console.log(typeof alumnos);
+//     localStorage.setItem("alumnos", JSON.stringify(alumnos));
+//     const datos = localStorage.getItem("alumnos");
+//     console.log(typeof datos);
+//     console.log(datos);
+//     const alumnosRecuperados = JSON.parse(datos);
+//     console.log(typeof(alumnosRecuperados));
+//     console.log(alumnosRecuperados);
+//     console.table(alumnosRecuperados);
+// }
 
-async function iniciar() {
-    const alumnos = await cargarAlumnos();
-    mostrarAlumnos(alumnos);
-}
+// async function iniciar() {
+//     const alumnos = await cargarAlumnos();
+//     mostrarAlumnos(alumnos);
+// }
 
-iniciar();
+// iniciar();
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -122,3 +122,47 @@ iniciar();
 //  iniciar()
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// TODO EL CODIGO ANTERIOR ES DE PRUEBA
+
+const formulario = document.querySelector("#formAlumno");
+
+formulario.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const nombre = document.querySelector("#nombre").value;
+    const carrera = document.querySelector("#carrera").value;
+    const email = document.querySelector("#email").value;
+    
+    const alumno = {
+        id: Date.now(),
+        nombre: nombre,
+        carrera: carrera,
+        email: email
+    }
+
+    const alumnos = obtenerAlumnos();
+    alumnos.push(alumno);
+
+    localStorage.setItem("alumnos", JSON.stringify(alumnos));
+
+    mostraAlumnos(alumnos);
+
+    formulario.reset();
+});
+
+function obtenerAlumnos() {
+    const datos = localStorage.getItem("alumnos")
+    if(datos) {
+        return JSON.parse(datos);
+    }
+    return []
+};
+
+const listaAlumnos = document.querySelector("#listaAlumnos");
+
+function mostraAlumnos(alumnos) {
+    listaAlumnos.innerHTML = "";
+    for(const alumno of alumnos) {
+        listaAlumnos.innerHTML += "<li>" + alumno.nombre + " - " + alumno.carrera + " - " + alumno.email + "</li>";
+    }
+}
