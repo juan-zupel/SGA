aa6b730a920209ac163aa1b957d9888fcd486f9c
juan_zupel <juanzupel21@gmail.com>
2026-08-21 11:22:17 -0300

    feat: actualización altas docentes, uso de localStrorage + tabla para mostrar registros

--- JS/docentes.js
+++ JS/docentes.js
@@ -1,70 +1,58 @@
-const mensaje = document.querySelector("#mensaje");
-const formulario2 = document.getElementById("formDocente");
+const formulario = document.getElementById("formDocente");
 const listaDocentes = document.getElementById("listaDocentes");
+let localExistenete = null;
 
-formulario2.addEventListener("submit", function (event) {
+formulario.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const nombreD = document.querySelector("#nombreD").value.trim();
-    const especialidad = document.querySelector("#especialidad").value.trim();
-    const correoD = document.querySelector("#correoD").value.trim();
+    const nombre = document.getElementById("nombreD").value.trim();
+    const especialidad = document.getElementById("especialidad").value.trim();
+    const correo = document.getElementById("correoD").value.trim();
+    const datos = obtenerDocentes();
 
-    const docente = {
+    const doc = {
         id: Date.now(),
-        nombre: nombreD,
+        nombre: nombre,
         especialidad: especialidad,
-        correo: correoD
+        correo: correo
     }
+    datos.push(doc);
+    localStorage.setItem("docentes", JSON.stringify(datos));
+    
+    mostrarEnTabla(datos);
 
-    localStorage.setItem("docentes", JSON.stringify(docente));
-
-    const docentess = obtenerDocentes();
-
-    mostrarDocentes(docentess);
-
-    formulario2.reset()
+    formulario.reset();
 });
 
 function obtenerDocentes() {
-    
-    const datos = localStorage.getItem("docentes");
-
-    if(datos) {
-        return JSON.parse(datos);
+    const data = localStorage.getItem("docentes");
+    if (data) {
+        return JSON.parse(data);
     }
-    return[];
-
+    return [];
 }
 
-function mostrarDocentes(docentes) {
+function mostrarEnTabla(docente) {
     listaDocentes.innerHTML = "";
-    for (const docente of docentes) {
-        listaDocentes.innerHTML += 
-        `
-        <tr>
-            <td>${docente.nombre}</td>, 
-            <td>${docente.especialidad}</td>, 
-            <td>${docente.correo}</td>
-            <td>
-                <button class= "btn-editar" data-id= "${docente.id}" title= "Editar docente">
-                    <i class= "fa-solid fa-pen"></i>
-                </button>
-            </td>
-            <td>
-                <button class= "btn-eliminar" data-id= "${docente.id}" title= "Eliminar Docente">
-                    <i class= "fa-solid fa-trash"></i>
-                </button>
-            </td>
-        </tr>
-        `;
+    for (const d of docente) {
+        listaDocentes.innerHTML =
+            `
+    <tr>
+        <td>${d.id}</td>
+        <td>${d.nombre}</td>
+        <td>${d.especialidad}</td>
+        <td>${d.correo}</td>
+        <td>
+            <button class="btn-editar" data-id="${d.id}" title="Editar Docente">
+                <i class= "fa-solid fa-pen"></i>
+            </button>
+        </td>
+        <td>
+            <button class="btn-eliminar" data-id="${d.id}" title="Eliminar Docente">
+                <i class= "fa-solid fa-trash"></i>
+            </button>
+        </td>
+    </tr>
+    `
     }
-}
-
-function mostrarmensaje(texto, tipo) {
-    mensaje.textContent = texto;
-    mensaje.className = tipo;
-    setTimeout(() => {
-        mensaje.textContent = " ";
-        mensaje.className = "oculto";
-    }, 3000);
 }
