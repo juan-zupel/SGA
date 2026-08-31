de62b450b02c6ae650dda0873b79fadaae800ead
juan_zupel <juanzupel21@gmail.com>
2026-08-31 15:42:09 -0300

    feat: Prueba de solicitudes GET y POST

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const alumnos = [
     {
@@ -37,6 +38,21 @@ app.get("/alumnos/:id", (req, res) => {
     const id = Number(req.params.id);
     const alumno = alumnos.find( a => a.id === id);
     res.json(alumno);
+});
+
+app.post("/alumnos", (req, res) => {
+    const nuevoAlumno = req.body;
+    alumnos.push(nuevoAlumno);
+    res.json({mensaje: "Alumno Registrado Correctamente"});
+}),
+
+app.put("/alumnos/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const alumno = alumnos.find(alumno => alumno.id === id);
+    alumno.id = req.body.id;
+    alumno.nombre = req.body.nombre;
+    alumno.carrera = req.body.carrera;
+    res.json({mensaje: "Alumno Actualizado Correctamente"});
 });
 
 app.listen(3000, () => {
